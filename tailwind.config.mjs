c28fd4572be6f03639f30410a5c5009c52342304
Rjshrivastav <shrivastavranjeet.rj@gmail.com>
2024-06-10 01:12:52 +0530

    process section done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,7 +7,10 @@ export default {
 		extend: {
 			fontFamily:{
 				raleway: ["Raleway Variable"]
-			}
+			},
+			boxShadow: {
+				'custom': '0 4px 8px 0 rgba(0, 0, 0, 5%), 0 6px 20px 0 rgba(0, 0, 0, 0%)',
+			  }
 		},
 	},
 	plugins: [],
