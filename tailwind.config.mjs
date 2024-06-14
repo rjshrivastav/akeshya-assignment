80a40fb1532f8f514a32c18874663f826950c67e
Rjshrivastav <shrivastavranjeet.rj@gmail.com>
2024-06-14 19:10:42 +0530

    project done

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -10,8 +10,17 @@ export default {
 			},
 			boxShadow: {
 				'custom': '0 4px 8px 0 rgba(0, 0, 0, 5%), 0 6px 20px 0 rgba(0, 0, 0, 0%)',
-			  }
+			  },
+			  keyframes: {
+				'up-down': {
+				  '0%': { transform: 'translateY(0)' },
+				  '100%': { transform: 'translateY(-8%)' },
+				},
+			  },
+			  animation: {
+				'up-down': 'up-down 2s ease-in-out infinite alternate-reverse both',
+			  },
 		},
-	},
 	plugins: [],
+}
 }
